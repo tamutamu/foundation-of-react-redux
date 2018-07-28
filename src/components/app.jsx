4ada627d5ef31662f4f081edd896f1b3c5bbe5c4
yanap <yanap1214@gmail.com>
2018-07-28 21:55:40 +0900

    add error case

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -10,22 +10,45 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-    }
+    };
+  }
+
+  setErrorMessage(message) {
+    this.setState({
+      address: message,
+      lat: 0,
+      lng: 0,
+    })
   }
 
   handlePlaceSubmit(place) {
     axios
-    .get(GEOCODDE_ENDPOINT, { params: { address: place } })
-    .then((results) => {
-      console.log(results);
-      const result = results.data.results[0];
-      const location = result.geometry.location;
-      this.setState({
-        address: result.formatted_address,
-        lat: location.lat,
-        lng: location.lng,
+      .get(GEOCODDE_ENDPOINT, { params: { address: place } })
+      .then((results) => {
+        console.log(results);
+        const data = results.data;
+        const result = data.results[0];
+        switch (data.status) {
+          case 'OK': {
+            const location = result.geometry.location;
+            this.setState({
+              address: result.formatted_address,
+              lat: location.lat,
+              lng: location.lng,
+            });
+            break;
+          }
+          case 'ZERO_RESULTS': {
+            this.setErrorMessage('結果が見つかりませんでした')
+            break;
+          }
+          default: {
+            this.setErrorMessage('エラーが発生しました');
+          }
+        }
+      }).catch((error) => {
+        this.setErrorMessage('通信に失敗しました');
       });
-    });
   }
 
   render() {

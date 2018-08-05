0e79fdfa0874f101709e04131bb6a549313b9572
yanap <yanap1214@gmail.com>
2018-08-06 01:47:03 +0900

    30.表示する情報の追加

--- src/domain/HotelRepository.js
+++ src/domain/HotelRepository.js
@@ -1,6 +1,10 @@
+import geolib from 'geolib';
+
 import Rakuten from '../lib/Rakuten';
 
 const RAKUTEN_APP_ID = '1043063542712502342';
+
+const DUMMY_IMG = 'https://stamp-mato.me/wp-content/uploads/2016/10/okaimono-panda_c.jpg';
 
 export const searchHotelByLocation = (location) => {
   const params = {
@@ -10,12 +14,24 @@ export const searchHotelByLocation = (location) => {
     longitude: location.lng,
   };
   return Rakuten.Travel.simpleHotelSearch(params)
-    .then(result => result.data.hotels.map((hotel) => {
+    .then(result =>
+      result.data.hotels.map((hotel) => {
         const basicInfo = hotel.hotel[0].hotelBasicInfo;
+        const thumbUrl = basicInfo.hotelThumbnailUrl;
+        const price = basicInfo.hotelMinCharge;
+        const distance = geolib.getDistance(
+          { latitude: location.lat, longitude: location.lng },
+          { latitude: basicInfo.latitude, longitude: basicInfo.longitude },
+        );
         return {
           id: basicInfo.hotelNo,
-        name: basicInfo.hotelName,
+          name: basicInfo.hotelName,
           url: basicInfo.hotelInformationUrl,
+          thumbUrl: thumbUrl ? thumbUrl : DUMMY_IMG,
+          price: price ? `${price}円` : '空室なし',
+          reviewCount: basicInfo.reviewCount,
+          distance,
         };
-      }  ),);
+      }),
+    );
 };

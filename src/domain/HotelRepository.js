b15d2f7f33c7fcdd669a77e69e33617338a2f62a
yanap <yanap1214@gmail.com>
2018-08-06 23:40:02 +0900

    add sort, until errors

--- src/domain/HotelRepository.js
+++ src/domain/HotelRepository.js
@@ -18,7 +18,6 @@ export const searchHotelByLocation = (location) => {
       result.data.hotels.map((hotel) => {
         const basicInfo = hotel.hotel[0].hotelBasicInfo;
         const thumbUrl = basicInfo.hotelThumbnailUrl;
-        const price = basicInfo.hotelMinCharge;
         const distance = geolib.getDistance(
           { latitude: location.lat, longitude: location.lng },
           { latitude: basicInfo.latitude, longitude: basicInfo.longitude },
@@ -28,7 +27,7 @@ export const searchHotelByLocation = (location) => {
           name: basicInfo.hotelName,
           url: basicInfo.hotelInformationUrl,
           thumbUrl: thumbUrl ? thumbUrl : DUMMY_IMG,
-          price: price ? `${price}円` : '空室なし',
+          price: basicInfo.hotelMinCharge,
           reviewCount: basicInfo.reviewCount,
           distance,
         };

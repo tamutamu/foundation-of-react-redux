0820e0284d8296366024dfcca60d382a80149c9a
yanap <yanap1214@gmail.com>
2018-08-21 18:51:19 +0900

    Redix-nize hotels

--- README.md
+++ README.md
@@ -226,3 +226,6 @@ http://redux.js.org/
 #### redux devtools
 
 http://extension.remotedev.io/
+
+
+### 
--- src/components/SearchPage.jsx
+++ src/components/SearchPage.jsx
@@ -1,33 +1,14 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import _ from 'lodash';
 import queryString from 'query-string';
 
 import SearchForm from '../containers/SearchForm';
 import GeocodeResult from './GeocodeResult';
 import Map from './Map';
-// import HotelsTable from './HotelsTable';
-
-const sortedHotels = (hotels, sortKey) => _.sortBy(hotels, h => h[sortKey]);
+import HotelsTable from './HotelsTable';
 
 class SearchPage extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      hotels: [
-      ],
-      sortKey: 'price',
-    };
-  }
-
-  componentDidMount() {
-    // const place = this.getPlaceParam();
-    // if (place) {
-    //   this.stateSearch(place);
-    // }
-  }
-
   getPlaceParam() {
     const params = queryString.parse(this.props.location.search);
     const place = params.place;
@@ -37,28 +18,11 @@ class SearchPage extends Component {
     return null;
   }
 
-  setErrorMessage(message) {
-    this.setState({
-      address: message,
-      location: {
-        lat: 0,
-        lng: 0,
-      },
-    });
-  }
-
   //  handlePlaceSubmit(e) {
   //    e.preventDefault();
   //    this.props.history.push(`\?place=${this.state.place}`);
   //    this.startSearch();
   //  }
-
-  handleSortKeyChange(sortKey) {
-    this.setState({
-      sortKey,
-      hotels: sortedHotels(this.state.hotels, sortKey),
-    });
-  }
 
   render() {
     return (
@@ -72,14 +36,8 @@ class SearchPage extends Component {
               address={this.props.geocodeResult.address}
               location={this.props.geocodeResult.location}
             />
-            {/*
             <h2>ホテル検索結果</h2>
-            <HotelsTable
-              hotels={this.state.hotels}
-              sortKey={this.state.sortKey}
-              onSort={sortKey => this.handleSortKeyChange(sortKey)}
-            />
-            */}
+            <HotelsTable />
           </div>
         </div>
       </div>
@@ -88,18 +46,19 @@ class SearchPage extends Component {
 }
 
 SearchPage.propTypes = {
-  //history: PropTypes.shape({ push: PropTypes.func }).isRequired,
+  // history: PropTypes.shape({ push: PropTypes.func }).isRequired,
   location: PropTypes.shape({ search: PropTypes.string }).isRequired,
-  geocodeResult: PropTypes.shape({ search: PropTypes.string }).isRequired,
-  address: PropTypes.string.isRequired,
-  location: PropTypes.shape({
-    lat: PropTypes.number.isRequired,
-    lng: PropTypes.number.isRequired,
-  }),
+  geocodeResult: PropTypes.shape({
+    address: PropTypes.string.isRequired,
+    location: PropTypes.shape({
+      lat: PropTypes.number.isRequired,
+      lng: PropTypes.number.isRequired,
+    }),
+  }).isRequired,
 };
 
 const mapStateToProps = state => ({
   geocodeResult: state.geocodeResult,
-})
+});
 
 export default connect(mapStateToProps)(SearchPage);
